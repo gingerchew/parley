8f0e19dbe9762a4c6390c50a78cdd358280ab700
ginger <26461046+gingerchew@users.noreply.github.com>
2025-01-19 12:05:50 -0500

    Sets up biome and adds examples dir

---
+++ packages/parley/index.ts
@@ -0,0 +1,6 @@
+import { AstroIntegration, AstroIntegrationLogger } from "astro";
+import { AstroError } from 'astro/errors';
+
+export default function ParleyIntegrations(userOpts):AstroIntegration {
+    
+}
